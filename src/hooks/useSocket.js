07612723563e7370d9c17353c6b1b6ae14b19f19
Hajjinni shaik <hajjishaik86@gmail.com>
2026-04-05 20:29:04 +0530

    al changes addede

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -10,7 +10,7 @@ export function useSocket(onNewsAlert, onDigestReady) {
     if (!user || socketRef.current) return;
 
     const SOCKET_URL =
-      process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
+      process.env.REACT_APP_SOCKET_URL;
 
     const socket = io(SOCKET_URL, {
       transports: ['websocket'],
